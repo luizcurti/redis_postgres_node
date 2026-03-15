6931431ca25589b4e68bd9b48ca4f245dc2941b7
Luiz Curti <luiz_curti@hotmail.com>
2026-03-15 17:54:08 +0000

    Changes

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '/src/server.ts'],
+  forceExit: true,
 };
